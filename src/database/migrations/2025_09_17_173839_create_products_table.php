d89154b8c8bf78213facedb3fbfa638e0049d468
kirin <fnagixjinf38@gmail.com>
2025-09-17 23:26:52 +0900

    20250917

---
+++ src/database/migrations/2025_09_17_173839_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('商品名');
+            $table->string('price')->comment('価格');
+            $table->string('brand')->comment('ブランド名');
+            $table->string('descriotion')->comment('商品説明');
+            $table->text('img');
+            $table->string('condition');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
