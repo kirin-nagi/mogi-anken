af7f69bb025b4fae3f60c59cb759e23a45440ae8
kirin <fnagixjinf38@gmail.com>
2025-10-03 21:11:13 +0900

    20251003

--- src/resources/views/merchandise/sell.blade.php
+++ src/resources/views/merchandise/sell.blade.php
@@ -10,6 +10,20 @@
     
             <div class="sell-form__content">
                 <div class="sell-form__heading">
+                    <h1>商品の出品</h1>
+                </div>
+                <div class="sell-form__subtitle">
+                    <h3>商品画像</h3>
+                </div>
+                <div style="border: gray dotted 4px;  padding: 15px;">
+                    <label for="upload">画像を選択する
+                        <input type="file" id="upload" class="hidden" accept="image/*">
+                    </label>
+                </div>
+                <div class="sell-form__title">
+                    <h2>商品の詳細</h2>
+                </div>
+                <div class="sell-form__title">
                     <h2>商品名と説明</h2>
                 </div>
                 <form class="form">
