53fa76d20f0296ae2db9651ff63de8952a7d58d1
kirin <fnagixjinf38@gmail.com>
2025-11-19 17:59:43 +0900

    20251119

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PurchaseRequest;
 use App\Models\Product;
 use App\Models\Address;
+use App\Http\Requests\AddressRequest;
 
 class PurchaseController extends Controller
 {
@@ -20,5 +21,24 @@ class PurchaseController extends Controller
         $address = Address::where('user_id', $user->id)->first();
 
         return view('merchandise.purchase', compact('product','address'));
+    }
+
+    public function showaddress(){
+
+        return view('user.address');
+    }
+
+    public function updateaddress(AddressRequest $request)
+    {
+        $user = Auth::user();
+
+        $address = Address::where('user_id', $user->id)->first();
+        $address->update([
+            'postcode'=> $request->postcode,
+            'address' => $request->address,
+            'building' => $request->building ?? null,
+        ]);
+
+        return redirect()->route('purchase', ['item_id' => $item_id]);
     }
 }
